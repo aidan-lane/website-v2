a927fe7fd222d2e7452dbf0afcb769107d4b5bcc
aidan-lane <aidanlane7@gmail.com>
2020-02-04 20:28:05 -0500

    frontend created

--- server/index.js
+++ server/index.js
@@ -8,6 +8,12 @@ app.use(cors());
 const routes = require('./routes.js');
 app.use('/api', routes);
 
+// handle production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(__dirname + '/public/'));
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log(`Started server on port ${port}`));
