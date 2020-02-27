c1998d642a311c75102c3e548932053776878dad
aidan-lane <aidanlane7@gmail.com>
2020-02-27 13:39:45 -0500

    created footer

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,12 +1,21 @@
 <template>
-  <div class="home">
+  <div id="home">
     <img alt="Vue logo" src="logo.png">
+    <p style="padding-top: 1em;">
+      Hello! My name is <Strong>Aidan Lane</Strong> — I am a 2nd year Computer Science major at Rensselaer Polytechnic Institute.
+    </P>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'home',
 };
 </script>
+
+<style scoped>
+  #home {
+    padding-top: 6%;
+    padding-bottom: 17%;
+  }
+</style>
